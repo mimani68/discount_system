88f3dd0180a3efc2c1232522c4321960fd676174
Administrator <imani@iran.ir>
2021-09-07 18:48:13 +0430

    wip: e2e test configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,20 @@ import { Module                 } from '@nestjs/common';
 import { TypeOrmModule          } from '@nestjs/typeorm';
 import { ConfigModule           } from '@nestjs/config';
 
-import { AppService                   } from 'src/app.service';
-import { AppController                } from 'src/app.controller';
-import { AuthenticateModule           } from 'src/authenticate/authenticate.module';
-import { ShopModule                   } from 'src/shop/shop.module';
-import { UsersModule                  } from 'src/users/users.module';
-import { BaseEntity as AuthBaseEntity } from 'src/authenticate/entities/base.entity';
-import { TokenEntity                  } from 'src/authenticate/entities/token.entity';
-import { VerificationCodeEntity       } from 'src/authenticate/entities/verificationcode.entity';
-import { BaseEntity as UserBaseEntity } from 'src/users/entities/base.entity';
-import { UserEntity                   } from 'src/users/entities/user.entity';
-import { BaseEntity as ShopBaseEntity } from 'src/shop/entities/base.entity';
-import { ProductEntity                } from 'src/shop/entities/product.entity';
-import { CategoryEntity               } from 'src/shop/entities/category.entity';
-import { DiscountEntity               } from 'src/shop/entities/discount.entity';
+import { AppService                   } from './app.service';
+import { AppController                } from './app.controller';
+import { AuthenticateModule           } from './authenticate/authenticate.module';
+import { ShopModule                   } from './shop/shop.module';
+import { UsersModule                  } from './users/users.module';
+import { BaseEntity as AuthBaseEntity } from './authenticate/entities/base.entity';
+import { TokenEntity                  } from './authenticate/entities/token.entity';
+import { VerificationCodeEntity       } from './authenticate/entities/verificationcode.entity';
+import { BaseEntity as UserBaseEntity } from './users/entities/base.entity';
+import { UserEntity                   } from './users/entities/user.entity';
+import { BaseEntity as ShopBaseEntity } from './shop/entities/base.entity';
+import { ProductEntity                } from './shop/entities/product.entity';
+import { CategoryEntity               } from './shop/entities/category.entity';
+import { DiscountEntity               } from './shop/entities/discount.entity';
 
 @Module({
   imports: [
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,13 +3,29 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 
 import { AppModule } from '../src/app.module';
+import { AuthenticateModule } from 'src/authenticate/authenticate.module';
+import { ShopModule } from 'src/shop/shop.module';
+import { UsersModule } from 'src/users/users.module';
+import { AppService } from 'src/app.service';
+import { AppController } from 'src/app.controller';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        AuthenticateModule,
+        ShopModule,
+        UsersModule
+      ],
+      controllers: [
+        AppController
+      ],
+      providers: [
+        AppService
+      ]
     }).compile();
 
     app = moduleFixture.createNestApplication();
