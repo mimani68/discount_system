a090050c668ba98d3a3fd9a364f74ae8a6dc5bc7
Administrator <imani@iran.ir>
2021-09-07 12:06:24 +0430

    refact(auth): remove old logics

--- src/authenticate/authenticate.module.ts
+++ src/authenticate/authenticate.module.ts
@@ -1,40 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AuthenticateService } from './authenticate.service';
-import { AuthenticateController } from './authenticate.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from 'src/users/users.module';
-import { NotificationModule } from 'src/notification/notification.module';
-import { VerificationCodeRepository } from './repositories/verificationcode.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TokenRepository } from './repositories/token.repository';
+
 import { JwtStrategy } from './jwt.strategy';
-import { UserCreditModule } from 'src/subscription/user-credit/user-credit.module';
-import { UserCreditService } from 'src/subscription/user-credit/user-credit.service';
-import { UserCreditEntity } from 'src/subscription/entities/user-credit.entity';
-import { VoucherUsageEntity } from 'src/subscription/entities/voucher-usage.entity';
-import { VoucherService } from 'src/subscription/voucher/voucher.service';
-import { VoucherEntity } from 'src/subscription/entities/voucher.entity';
-import { AccountTypeService } from 'src/subscription/account-type/account-type.service';
-import { AccountTypeEntity } from 'src/subscription/entities/accounttype.entity';
+import { TokenRepository } from './repositories/token.repository';
+import { VerificationCodeRepository } from './repositories/verificationcode.repository';
+import { AuthenticateService } from './authenticate.service';
+import { AuthenticateController } from './authenticate.controller';
 
 
 @Module({
-  imports: [UserCreditModule,
-    TypeOrmModule.forFeature([VerificationCodeRepository, TokenRepository,
-      UserCreditEntity, VoucherUsageEntity, VoucherEntity, AccountTypeEntity]),
-    NotificationModule,
-    UsersModule,
+  imports: [
+    TypeOrmModule.forFeature([VerificationCodeRepository, TokenRepository]),
     PassportModule.register({
       defaultStrategy: 'jwt'
     }),
     JwtModule.register({
-      secret: '76fc673b7bf2c432f8ef813bd970c5f45b1515fca0d805d8d5b045435ad1f9e1',
+      secret: process.env.SECRET,
       signOptions: {
-        expiresIn: '40w'
+        expiresIn: process.env.JWT_LIFETIME
       }
     })],
-  providers: [AuthenticateService, JwtStrategy, UserCreditService, VoucherService, AccountTypeService],
+  providers: [AuthenticateService, JwtStrategy],
   controllers: [AuthenticateController],
   exports: [JwtStrategy, PassportModule]
 })
